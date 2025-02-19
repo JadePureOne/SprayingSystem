9ad1aab301b9d7867bbcf881e4ca0441d4816a6d
hook <jadepureone@outlook.com>
2025-02-19 11:07:50 +0800

    ✨ feat(UIExtension): 主题切换

---
+++ SprayingSystem/UIExtension.cs
@@ -0,0 +1,18 @@
+﻿using Sunny.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprayingSystem
+{
+    public static class UIExtension
+    {
+        /// <summary>
+        /// 委托通知主题切换
+        /// </summary>
+        public static Action<UIStyleManager> SetStyleManager;
+
+    }
+}
